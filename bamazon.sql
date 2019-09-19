14eb90fa404d294ae9f1987fbd59cebc24e66fd2
Sonny D <damicose14@gmail.com>
2019-09-19 09:59:09 -0500

    slight adjustment, basic framework for js file

--- bamazon.sql
+++ bamazon.sql
@@ -7,7 +7,7 @@ USE bamazon_DB;
 CREATE TABLE products (
     item_id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(30) NULL,
-    department_name VARCHAR (30) NULL
+    department_name VARCHAR (30) NULL,
     price DECIMAL (10,2) NULL,
     stock_quantity INT NULL,
     PRIMARY KEY (item_id)
