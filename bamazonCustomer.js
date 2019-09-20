e1f84ab844bd39a0294ce2057ce8423c83faff2a
Sonny D <damicose14@gmail.com>
2019-09-19 19:40:23 -0500

    getting closer to unlocking the secrets of communicating with the daggone sql

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,107 +15,79 @@ const connection = mysql.createConnection({
   database: "bamazon_DB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
   console.log("Displaying Product Array")
   readProducts();
   inquirer
-  .prompt([{
-    type: "input",
-    message: "Enter the item_id of the product you'd like to purchase.",
-    name: "whichProd"
-  },
-{
-  type: "input",
-  message: "How many would you like to purchase?",
-  name: "howMany"
-}
-])
-.then(function(inquirerResponse) {
-  let prodQuan = (
-    connection.query(
-"SELECT FROM "
-    )
-  )
-if (inquirerResponse.howMany > prodQuan) {
-  console.log("Error: Desired Quantity is greater than Available Stock")
-}
-else {
-  console.log("Purchase successful! Adjusting stock levels...")
-  updateProduct();
-}
-
-});
-
-function readProducts() {
-  connection.query("SELECT * FROM products", function(err, res) {
-    if (err) throw err;
-    // Log all results of the SELECT statement
-    console.log(res);
-    // connection.end();
-  });
-}
-
-function updateProduct() {
-  // console.log("Updating all Rocky Road quantities...\n");
-  connection.query(
-    "UPDATE products SET ? WHERE ?",
-    [
-      {
-        stock_quantity: (parseInt(stock_quantity) - parseInt(inquirerResponse.howMany))
+    .prompt([{
+        type: "input",
+        message: "Enter the item_id of the product you'd like to purchase.",
+        name: "whichProd"
       },
       {
-        item_id: inquirerResponse.whichProd
+        type: "input",
+        message: "How many would you like to purchase?",
+        name: "howMany"
       }
-    ],
-    function(err, res) {
+    ])
+    .then(function (answer) {
+      connection.query(
+        // With assistance from SO (https://stackoverflow.com/questions/20819826/how-to-pass-parameters-to-mysql-query-callback-in-nodejs)
+        `SELECT * FROM products`,
+        function (err, res) {
+          console.log(res[(answer.howMany - 1)].stock_quantity);
+          // console.log(res);
+          console.log(res[(answer.howMany - 1)]);
+          if (answer.howMany > res[(answer.howMany - 1)].stock_quantity) {
+            console.log("Error: Desired Quantity is greater than Available Stock")
+            connection.end();
+          } else {
+            console.log("Purchase successful! Adjusting stock levels...")
+            updateProduct();
+          }
+
+
+
+
+          function updateProduct() {
+            // console.log("Updating all Rocky Road quantities...\n");
+            connection.query(
+              "UPDATE products SET ? WHERE ?",
+              [{
+                  stock_quantity: (parseInt(res) - parseInt(answer.howMany))
+                },
+                {
+                  item_id: answer.whichProd
+                }
+              ],
+              function (err, res) {
+                if (err) throw err;
+                console.log("Update Successful! Thank you for shopping with Bamazon~~~");
+                // Call deleteProduct AFTER the UPDATE completes
+                // deleteProduct();
+              }
+            );
+          }
+
+
+
+
+
+
+        }
+      )
+
+
+    });
+
+  function readProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
       if (err) throw err;
-      console.log("Update Successful! Thank you for shopping with Bamazon~~~");
-      // Call deleteProduct AFTER the UPDATE completes
-      // deleteProduct();
-    }
-  );
-
-// function createProduct() {
-//   console.log("Inserting a new product...\n");
-//   let query = connection.query(
-//     "INSERT INTO products SET ?",
-//     {
-//       flavor: "Rocky Road",
-//       price: 3.0,
-//       quantity: 50
-//     },
-//     function(err, res) {
-//       if (err) throw err;
-//       console.log(res.affectedRows + " product inserted!\n");
-//       // Call updateProduct AFTER the INSERT completes
-//       updateProduct();
-//     }
-//   );
-
-//   // logs the actual query being run
-//   console.log(query.sql);
-// }
-
-
-
-//   // logs the actual query being run
-//   console.log(query.sql);
-// }
-
-// function deleteProduct() {
-//   console.log("Deleting all strawberry icecream...\n");
-//   connection.query(
-//     "DELETE FROM products WHERE ?",
-//     {
-//       flavor: "strawberry"
-//     },
-//     function(err, res) {
-//       if (err) throw err;
-//       console.log(res.affectedRows + " products deleted!\n");
-//       // Call readProducts AFTER the DELETE completes
-//       readProducts();
-//     }
-//   );
-// }
+      // Log all results of the SELECT statement
+      console.log(res);
+      // connection.end();
+    });
+  }
+})
